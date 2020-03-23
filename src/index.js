3eb2412ad453ff2fed06bfdde7fcf7063627315c
Amit Kumar <akumar@indeed.com>
2020-03-24 01:07:57 +0530

    Creating reducer and adding it to store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import postReducer from './reducer/postReducer'
+
+import { createStore } from 'redux';
+
+const store = createStore(postReducer);
 
 ReactDOM.render(
     <App />,document.getElementById('root'));
---
+++ src/reducer/postReducer.js
@@ -0,0 +1,10 @@
+const postreducer = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_POST':
+            return state.concat([action.data]);
+        default:
+            return state;
+    }
+}
+
+export default postreducer;
