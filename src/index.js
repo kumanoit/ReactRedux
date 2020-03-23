cbc5334ba5ea57cbc5ca35df5b897aec96d4fa2f
Amit Kumar <akumar@indeed.com>
2020-03-24 01:11:46 +0530

    Providing store to all components in app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import postReducer from './reducer/postReducer'
+import { Provider } from 'react-redux';
 
 import { createStore } from 'redux';
 
 const store = createStore(postReducer);
 
 ReactDOM.render(
-    <App />,document.getElementById('root'));
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
