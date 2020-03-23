703afe0c9df84fcfa94964d987ee7126f343d700
Amit Kumar <akumar@indeed.com>
2020-03-24 01:43:51 +0530

    Mapping state inside store to props

--- src/AllPost.js
+++ src/AllPost.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class AllPost extends Component {
     render() {
-        return(
+        return (
             <div>
                 <h1>All Posts</h1>
+                {console.log(this.props.posts)}
             </div>
         );
     }
 }
 
-export default AllPost;
+const mapStateToProps = (state) => {
+    return {
+        posts: state,
+    };
+};
+
+export default connect(mapStateToProps)(AllPost);
